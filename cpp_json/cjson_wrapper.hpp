b7a1817845e22aff54428415fe41bf4b06b81b62
coiled-coil <nautilus0808@gmail.com>
2011-07-02 23:05:33 +0900

    Add cJSON C++ wrapper, and Objective-C addition.

---
+++ cpp_json/cjson_wrapper.hpp
@@ -0,0 +1,189 @@
+//
+//  Copyright 2011 Kaoru Yanase. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#pragma once
+#include <cJSON.h>
+#include "detail/safe_bool.hpp"
+#include <tr1/memory>
+#include <boost/iterator_adaptors.hpp>
+#include <boost/optional.hpp>
+#include <stdexcept>
+#include <string>
+
+namespace cpp_json {
+
+    struct json_parse_error : public std::runtime_error
+    {
+        /** Takes a character string describing the error.  */
+        explicit json_parse_error(const std::string& msg)
+        :   std::runtime_error(msg)
+        {
+        }
+    };
+
+    // Customization point for library developers
+    template <typename ResultType>
+    class cjson_cast_impl
+    {
+    public:
+        static boost::optional<ResultType> get(cJSON *json)
+        {
+            return ResultType::__UNDEFINED_TYPE_ERROR__;
+        }
+    };
+
+    // Public interface for users
+    template <typename ResultType>
+    boost::optional<ResultType> cjson_cast(cJSON *json)
+    {
+        return cjson_cast_impl<ResultType>::get(json);
+    }
+
+    // --- Some specializations for well-known types
+
+    // const char *
+    template <>
+    class cjson_cast_impl<const char *>
+    {
+    public:
+        static boost::optional<const char*> get(cJSON *value)
+        {
+            if (value) {
+                if (value->type == cJSON_String) {
+                    return value->valuestring;
+                }
+            }
+            return boost::optional<const char*>();
+        }
+    };
+
+    // int
+    template <>
+    class cjson_cast_impl<int>
+    {
+    public:
+        static boost::optional<int> get(cJSON *value)
+        {
+            if (value) {
+                if (value->type == cJSON_Number) {
+                    return value->valueint;
+                }
+            }
+            return boost::optional<int>();
+        }
+    };
+
+    class cjson_wrapper
+    : public detail::safe_bool<cjson_wrapper>
+    {
+        cJSON *json_;
+
+    public:
+        struct iterator
+        : boost::iterator_adaptor<
+                    iterator,
+                    cJSON*,                         // base type
+                    cjson_wrapper,                  // value type
+                    boost::forward_traversal_tag    // iterator traits
+        >
+        {
+            iterator( cJSON* p )
+            : boost::iterator_adaptor<iterator,cJSON*,cjson_wrapper,boost::forward_traversal_tag>( p ) {}
+            void increment()         { base_reference() = base()->next; }
+            cjson_wrapper dereference() const { return cjson_wrapper(base_reference()); }
+        };
+
+        iterator begin(){ return iterator(json_->child); }
+        iterator end()  { return iterator(NULL); }
+
+    public:
+        explicit cjson_wrapper(cJSON *json)
+        :   json_(json)
+        {
+        }
+
+        template <typename RetType>
+        RetType as() const
+        {
+            if (boost::optional<RetType> v = cjson_cast<RetType>(json_)) {
+                return *v;
+            }
+            else {
+                throw json_parse_error("JSON parse error");
+            }
+        }
+
+        template <typename RetType>
+        RetType get_or_default(RetType const& default_value) const
+        {
+            if (boost::optional<RetType> v = cjson_cast<RetType>(json_)) {
+                return *v;
+            }
+            else {
+                return default_value;
+            }
+        }
+
+        template <typename RetType>
+        boost::optional<RetType> get_optional() const
+        {
+            return cjson_cast<RetType>(json_);
+        }
+
+        size_t size() const
+        {
+            return cJSON_GetArraySize(json_);
+        }
+
+        cjson_wrapper operator[](size_t index) const
+        {
+            return cjson_wrapper(cJSON_GetArrayItem(json_, index));
+        }
+
+        cjson_wrapper operator[](const char *key) const
+        {
+            return cjson_wrapper(cJSON_GetObjectItem(json_, key));
+        }
+
+    public:
+        // Implementation for safe_bool CRTP
+        bool boolean_test() const
+        {
+            return json_ != 0 && json_->type != cJSON_NULL && json_->type != cJSON_False;
+        }
+    };
+
+    class cjson_document : public cjson_wrapper
+    {
+        std::tr1::shared_ptr<cJSON> json_;
+    public:
+        explicit cjson_document(cJSON *json)
+        :   json_(json, cJSON_Delete)
+        ,   cjson_wrapper(json)
+        {
+        }
+
+        static cjson_document parse(const char *str)
+        {
+            return cjson_document(cJSON_Parse(str));
+        }
+
+        static cjson_document parse(NSString *str)
+        {
+            return cjson_document(cJSON_Parse([str UTF8String]));
+        }
+    };
+} // namespace cpp_json
---
+++ cpp_json/objc.hpp
@@ -0,0 +1,39 @@
+//
+//  Copyright 2011 Kaoru Yanase. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#pragma once
+#import <Foundation/Foundation.h>
+#include "cjson_wrapper.hpp"
+
+namespace cpp_json {
+
+    // NSString *
+    template <>
+    class cjson_cast_impl<NSString *>
+    {
+    public:
+        static boost::optional<NSString*> get(cJSON *value)
+        {
+            if (value) {
+                if (value->type == cJSON_String) {
+                    return [NSString stringWithUTF8String:value->valuestring];
+                }
+            }
+            return boost::optional<NSString*>();
+        }
+    };
+
+} // namespace cpp_json
